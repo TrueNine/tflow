c279c133365becaba8543a60296a0fccd9ed2599
TrueNine <truenine@163.com>
2022-06-13 23:03:13 +0800

    init 尝试 nuxt

---
+++ tui/tflowpcssr/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <hello-word/>
+  </div>
+</template>
+<script>
+import HelloWord from "./components/HelloWord";
+
+export default {
+  components: {HelloWord}
+}
+</script>
---
+++ tui/tflowpcssr/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <hello-word/>
+</template>
+
+<script>
+import HelloWord from "../components/HelloWord";
+
+export default {
+  name: "index",
+  components: {HelloWord}
+}
+</script>
+
+<style scoped>
+
+</style>
